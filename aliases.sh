e75b4e7ffb2ed930495ae8bd4913b1a392f12492
errozz <errozz@gmail.com>
2026-01-09 23:28:17 +0500

    Add aliases for Clash Config Generator in aliases.sh

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+# Aliases for Clash Config Generator
+alias clash-sync='/opt/clash-proxy-rules-gen/config-generator/.venv/bin/python /opt/clash-proxy-rules-gen/config-generator/sync_configs.py'
+alias clash-rotate='/opt/clash-proxy-rules-gen/config-generator/.venv/bin/python /opt/clash-proxy-rules-gen/config-generator/rotate_settings.py'
